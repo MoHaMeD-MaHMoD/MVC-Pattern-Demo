04577a23cb4b6e2b2e552e3a685a62adce6b7790
MoHaMeD-MaHMoD <MohamedAbdElTawab2212@gmail.com>
2019-12-12 17:14:51 +0200

    add butter knife

--- app/src/main/java/com/example/mvcpatterndemo/ui/MainActivity.java
+++ app/src/main/java/com/example/mvcpatterndemo/ui/MainActivity.java
@@ -1,35 +1,44 @@
 package com.example.mvcpatterndemo.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.mvcpatterndemo.R;
 import com.example.mvcpatterndemo.pojo.data;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
-TextView nameTextView,ageTextview;
-Button clickbutton;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.button)
+    Button clickbutton;
+    @BindView(R.id.age)
+    TextView ageTextview;
+    @BindView(R.id.name)
+    TextView nameTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nameTextView=findViewById(R.id.name);
-        ageTextview=findViewById(R.id.age);
-        clickbutton=findViewById(R.id.button);
+        ButterKnife.bind(this);
+
         clickbutton.setOnClickListener(this);
     }
-    public data getModelFromDataBase (){
-        return new data("Mohamed Mahmoud",23);
-   }
 
-   public void getData(){
+    public data getModelFromDataBase() {
+        return new data("Mohamed Mahmoud", 23);
+    }
+
+    public void getData() {
         nameTextView.setText(getModelFromDataBase().getName());
         ageTextview.setText(String.valueOf(getModelFromDataBase().getAge()));
-   }
+    }
 
     @Override
     public void onClick(View v) {
